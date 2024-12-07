4419a26759f8bcbe556e66b4ef4e2f5fdb80192d
subhash131 <subhashnayak131@gmail.com>
2024-12-08 00:15:33 +0530

    update hero section

--- src/components/landing-page/hero/features.tsx
+++ src/components/landing-page/hero/features.tsx
@@ -1,54 +1,50 @@
 import Image from "next/image";
 import React from "react";
 
+const features = [
+  {
+    name: "feature1",
+    image: "/aquila.webp",
+  },
+  {
+    name: "feature2",
+    image: "/birla.webp",
+  },
+  {
+    name: "feature3",
+    image: "/creative.webp",
+  },
+  {
+    name: "feature4",
+    image: "/nomedia.webp",
+  },
+  {
+    name: "feature5",
+    image: "/persist.webp",
+  },
+];
+
 const Features = () => {
   return (
     <div className="size-full flex items-center justify-center py-10">
-      <div className="size-40 rounded-xl p-1 bg-gray-200 rotate-12">
-        <Image
-          src="/aquila.webp"
-          alt="feature1"
-          className="size-full rounded-lg overflow-hidden bg-white"
-          width={160}
-          height={160}
-        />
-      </div>
-      <div className="size-40 rounded-xl p-1 bg-gray-200 -rotate-12">
-        <Image
-          src="/birla.webp"
-          alt="feature1"
-          className="size-full rounded-lg overflow-hidden bg-white"
-          width={160}
-          height={160}
-        />
-      </div>
-      <div className="size-40 rounded-xl p-1 bg-gray-200 rotate-12">
-        <Image
-          src="/creative.webp"
-          alt="feature1"
-          className="size-full rounded-lg overflow-hidden bg-white"
-          width={160}
-          height={160}
-        />
-      </div>
-      <div className="size-40 rounded-xl p-1 bg-gray-200 -rotate-12">
-        <Image
-          src="/nomedia.webp"
-          alt="feature1"
-          className="size-full rounded-lg overflow-hidden bg-white"
-          width={160}
-          height={160}
-        />
-      </div>
-      <div className="size-40 rounded-xl p-1 bg-gray-200 rotate-12">
-        <Image
-          src="/persist.webp"
-          alt="feature1"
-          className="size-full rounded-lg overflow-hidden bg-white"
-          width={160}
-          height={160}
-        />
-      </div>
+      {features.map(({ image, name }, idx) => {
+        return (
+          <div
+            className={`size-40 rounded-xl p-1 bg-gray-200 ${
+              idx % 2 === 0 ? "-rotate-12" : "rotate-12"
+            }`}
+            key={name}
+          >
+            <Image
+              src={image}
+              alt={name}
+              className="size-full rounded-lg overflow-hidden bg-white"
+              width={160}
+              height={160}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
